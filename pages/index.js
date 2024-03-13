3bdf82fa7a6d861a73d0056c11988735ffc993ba
Chase Cole <chasewcole@gmail.com>
2024-03-13 15:15:50 -0700

    Removing Header component. Adding and styling Navigation component.

--- pages/index.js
+++ pages/index.js
@@ -2,7 +2,6 @@ import Link from 'next/link';
 import { getPosts } from '../utils/mdx-utils';
 
 import Footer from '../components/Footer';
-import Header from '../components/Header';
 import Layout, { GradientBackground } from '../components/Layout';
 import ArrowIcon from '../components/ArrowIcon';
 import { getGlobalData } from '../utils/global-data';
@@ -12,7 +11,6 @@ export default function Index({ posts, globalData }) {
   return (
     <Layout>
       <SEO title={globalData.name} description={globalData.blogTitle} />
-      <Header name={globalData.name} />
       <main className="w-full">
         <h1 className="text-3xl lg:text-5xl text-center mb-12">
           {globalData.blogTitle}
