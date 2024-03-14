ad525416763014ebfd3095fee4139d081b59adff
Chase Cole <chasewcole@gmail.com>
2024-03-13 19:08:32 -0700

    Fixing Nextjs Image import

--- components/Navigation.js
+++ components/Navigation.js
@@ -1,7 +1,7 @@
 import { Fragment } from 'react';
 import { Disclosure, Menu, Transition } from '@headlessui/react';
 import { Bars3Icon, BellIcon, XMarkIcon } from '@heroicons/react/24/outline';
-import Image from 'next/Image';
+import Image from 'next/image';
 
 const navigation = [
   { name: 'Home', href: '#', current: true },
